e07e23054f0df9b62c169add564ab94cd38fc3ca
Mikhail Kropachev <mishakro@yandex.ru>
2024-06-06 21:08:31 +0300

    Первая программа

--- index.pas
+++ index.pas
@@ -1,1 +1,15 @@
-﻿
+﻿type
+    mass = record 
+    x : integer;
+    y : real;
+    end;
+var a : mass;
+res : real;
+begin
+    write('Введите x (integer): ');
+    readln(a.x);
+    write('Введите y (real): ');
+    readln(a.y);
+    res := a.y * a.x;
+    writeln(res:0:2);
+end.
